cf7383b040be379526b0192b50a8729aa74ac016
samie105 <samsonrichfield@gmail.com>
2024-07-20 00:07:17 +0100

    pushed the data like that

--- trackpage.js
+++ trackpage.js
@@ -35,6 +35,11 @@ async function fetchPackageDetails(trackingId) {
       return;
     }
 
+    // Filter out empty checkpoints
+    const nonEmptyCheckpoints = packageData.checkpoints.filter(
+      (checkpoint) => checkpoint.trim() !== ""
+    );
+
     // Template for displaying package details
     const packageDetailsTemplate = `
       <div class="flex flex-col space-y-6 mt-10 /p-6">
@@ -58,46 +63,81 @@ async function fetchPackageDetails(trackingId) {
             </div>
           </div>
           <div>
-            <div class="text-black/50 text-2xl mt-12 font-bold text-gray-700">Past Checkpoints</div>
-            <div class="space-y-10 mt-6">
-              ${packageData.checkpoints
-                .map(
-                  (checkpoint, index) => `
-                    <div class="flex items-center space-x-4">
-                       <div class="flex items-center relative justify-center p-4 rounded-full bg-[#FFB400] text-white">
-                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-7">
-                               <path fill-rule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clip-rule="evenodd" />
-                              </svg>
+          <div class="border p-5 rounded-md">
+                        <div class="text-black/50 text-2xl text-gray-700">About Package</div>
+                                      <div class="font-bold text-2xl mt-3">${
+                                        packageData.packageDescription
+                                      }</div>
 
+
+          </div>
+          <div class="border p-5 rounded-md mt-7">
+            <div class="text-black/50  text-2xl  font-bold text-gray-700">Past Checkpoints</div>
+            <div class="space-y-10 mt-6">
+              ${
+                nonEmptyCheckpoints.length > 0
+                  ? nonEmptyCheckpoints
+                      .map(
+                        (checkpoint, index) => `
+                    <div class="flex items-center space-x-4">
+                       <div class="flex items-center relative justify-center p-4 z-10 rounded-full ${
+                         checkpoint.toLowerCase().includes("awaiting") ||
+                         packageData.packageStatus
+                           .toLowerCase()
+                           .includes("on hold")
+                           ? "bg-[#FFB400]"
+                           : "bg-green-600"
+                       }  text-white">
+                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-7">
+                               <path d="M2.908 2.067A.978.978 0 0 0 2 3.05V8h6V3.05a.978.978 0 0 0-.908-.983 32.481 32.481 0 0 0-4.184 0ZM12.919 4.722A.98.98 0 0 0 11.968 4H10a1 1 0 0 0-1 1v6.268A2 2 0 0 1 12 13h1a.977.977 0 0 0 .985-1 31.99 31.99 0 0 0-1.066-7.278Z" />
+                               <path d="M11 13a1 1 0 1 1-2 0 1 1 0 0 1 2 0ZM2 12V9h6v3a1 1 0 0 1-1 1 2 2 0 1 0-4 0 1 1 0 0 1-1-1Z" />
+                               <path d="M6 13a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z" />
+                             </svg>
                              ${
-                               index < packageData.checkpoints.length - 1
-                                 ? `<div class="dottener absolute h-14 rounded-full w-1 border-dashed border border-[#FFB400] -bottom-12"></div>`
+                               index < nonEmptyCheckpoints.length - 1
+                                 ? `<div class="dottener -z-10 absolute h-14 ${
+                                     checkpoint
+                                       .toLowerCase()
+                                       .includes("awaiting") ||
+                                     packageData.packageStatus
+                                       .toLowerCase()
+                                       .includes("on hold")
+                                       ? "border-[#FFB400]"
+                                       : "border-green-600"
+                                   }  rounded-full w-1 border-dotted border border-[#FFB400] -bottom-12"></div>`
                                  : ""
                              }
-                     
                       </div>
                       <div>
                         <div class="text-gray-900 text-2xl">${checkpoint}</div>
                       </div>
                     </div>
                   `
-                )
-                .join("")}
+                      )
+                      .join("")
+                  : `<div class="text-gray-500 text-center">No checkpoints reached yet.</div>`
+              }
+            </div>
             </div>
           </div>
+          <div class="border p-5 rounded-md mt-">
           <div>
-            <div class="text-black/50 text-2xl font-bold text-gray-700 mt-5">Current Location</div>
+            <div class="text-black/50 text-2xl font-bold text-gray-700 ">Current Location</div>
             <div class="flex items-center space-x-4 mt-3">
-              <div class="flex items-center justify-center p-4 rounded-full bg-primary text-white">
+              <div class="flex items-center justify-center p-4 rounded-full ${
+                packageData.packageStatus.toLowerCase() === "delivered"
+                  ? "bg-green-600"
+                  : "bg-[#FFB400]"
+              } text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-7">
-  <path fill-rule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clip-rule="evenodd" />
-</svg>
-
+                  <path fill-rule="evenodd" d="m7.539 14.841.003.003.002.002a.755.755 0 0 0 .912 0l.002-.002.003-.003.012-.009a5.57 5.57 0 0 0 .19-.153 15.588 15.588 0 0 0 2.046-2.082c1.101-1.362 2.291-3.342 2.291-5.597A5 5 0 0 0 3 7c0 2.255 1.19 4.235 2.292 5.597a15.591 15.591 0 0 0 2.046 2.082 8.916 8.916 0 0 0 .189.153l.012.01ZM8 8.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3Z" clip-rule="evenodd" />
+                </svg>
               </div>
               <div>
                 <div class="font-bold text-2xl">${packageData.currentLocation.toUpperCase()}</div>
               </div>
             </div>
+          </div>
           </div>
         </div>
       </div>
